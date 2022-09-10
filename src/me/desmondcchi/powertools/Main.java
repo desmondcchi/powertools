227c49b3470fd98f280bc1f22b1a1ffd06d385f6
Desmond Chi <dcchi@live.com>
2022-09-10 12:09:35 -0700

    Implemented functionality of powertools!

--- src/me/desmondcchi/powertools/Main.java
+++ src/me/desmondcchi/powertools/Main.java
@@ -3,11 +3,16 @@ package me.desmondcchi.powertools;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import me.desmondcchi.powertools.commands.Powertool;
+import me.desmondcchi.powertools.listeners.PowertoolClick;
 
 public class Main extends JavaPlugin {
 	@Override
 	public void onEnable() {
+		saveDefaultConfig();
+		
 		this.getCommand("powertool").setExecutor(new Powertool(this));
+		
+		this.getServer().getPluginManager().registerEvents(new PowertoolClick(this), this);
 	}
 	
 	@Override
--- src/me/desmondcchi/powertools/listeners/PowertoolClick.java
+++ src/me/desmondcchi/powertools/listeners/PowertoolClick.java
@@ -3,7 +3,9 @@ package me.desmondcchi.powertools.listeners;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 
 public class PowertoolClick implements Listener {
@@ -12,8 +14,18 @@ public class PowertoolClick implements Listener {
 	public PowertoolClick(Plugin plugin) {
 		this.plugin = plugin;
 	}
+	
 	@EventHandler
 	public void onClick(PlayerInteractEvent event) {
-		Player player = event.getPlayer();
+		if (event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK) {
+			Player player = event.getPlayer();
+			
+		    ItemStack item = player.getInventory().getItemInMainHand();
+		    String itemName = item.getType().name();
+
+		    if (plugin.getConfig().isSet(player.getUniqueId().toString() + "." + itemName)) {
+		    	player.performCommand(plugin.getConfig().getString(player.getUniqueId().toString() + "." + itemName));
+		    }
+		}
 	}
 }
